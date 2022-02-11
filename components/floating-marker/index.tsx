b51806c1548ac9bf65a82dcaee7b3b9cb3846e43
Emil Tayeb <emiltayeb912@gmail.com>
2022-02-11 16:48:09 +0200

    init iwht slate

---
+++ components/floating-marker/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+export interface IFloatingMenu {
+  markerState: {
+    directions: DOMRect;
+    offset: number;
+    functionality?: 'add' | 'remove';
+  };
+  onMarkerClick: any;
+}
+
+const StyledFloatingMenu = styled.div<IFloatingMenu | any>`
+  position: absolute;
+  top: ${({ position }) =>
+    position ? position.bottom + window.pageYOffset + 4 : -10000}px;
+  left: ${({ position }) =>
+    position ? position.left + window.pageXOffset - 5 : -1000}px;
+  z-index: 100;
+
+  & button {
+    background: ${({ state }) =>
+      state === 'add' ? `var(--green-5)` : `var(--red-5)`};
+    color: white;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: background 0.5s ease;
+    user-select: none;
+    &:hover {
+      background: white;
+      color: var(--green-5);
+    }
+  }
+`;
+export const FloatingMenu: React.FC<IFloatingMenu> = ({
+  markerState,
+  onMarkerClick,
+}) => {
+  return ReactDOM.createPortal(
+    <StyledFloatingMenu
+      data-editor-marker='true'
+      position={markerState.directions}
+      state={markerState.functionality}
+      onClick={onMarkerClick}>
+      <button type='button'>
+        {markerState.functionality === 'add' ? '+' : '-'}
+      </button>
+    </StyledFloatingMenu>,
+    document.body
+  );
+};
