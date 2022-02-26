86a4876f229297506ded23e1f9361c95172d7d4a
Emil Tayeb <emilta@avantisteam.com>
2022-02-26 10:32:07 +0200

    Added [cardId] route

--- pages/editor/[cardId].tsx
+++ pages/editor/[cardId].tsx
@@ -1,17 +1,33 @@
+import { where } from 'firebase/firestore';
 import { useRouter } from 'next/router';
 import React from 'react'
+import { useUser } from 'reactfire';
+import Editor from '../../components/editor';
+import { CardType } from '../../components/editor/types';
+import useGetData from '../../utils/useGetData';
 
 const UserCard = function () {
 
 	const params = useRouter().query
+	const { data: user } = useUser();
+	const { db, resultData, dataStatus } = useGetData({
+		dataBaseName: 'users',
+		options: [where('email', '==', user?.email || '')],
+	});
 
-	console.log(params)
+	const card = resultData?.cards.find?.((card: CardType) => card.id === params.cardId)
+
+
 	//  user want to load a specific card (like smart links)
 	//  check if its in the store already
 	// if not - fetch it
 
 	return <div>
-		{JSON.stringify(params)}
+		<Editor
+			mode='editing'
+			card={card}
+			onSaveCard={(card) => console.log(card)}
+		/>
 	</div>
 }
 
