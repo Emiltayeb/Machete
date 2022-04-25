7621a3dcb048e52c749b3442ba95e221e9d07993
Emil Tayeb <emilta@avantisteam.com>
2022-04-25 23:01:42 +0300

    fixed diff

--- utils/getStringDiffrences.ts
+++ utils/getStringDiffrences.ts
@@ -1,7 +1,7 @@
 export const findDiff = (correctAns: string, checkAnswer: string) => {
 	let diffs = "";
 	correctAns.split("").forEach(function (el, index) {
-		if (checkAnswer[index] !== el) {
+		if (checkAnswer[index].toLocaleLowerCase?.() !== el.toLowerCase?.()) {
 			diffs += `${el},`
 		}
 	})
